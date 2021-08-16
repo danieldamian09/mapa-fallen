5d255fae6640e70c1a8f3c6f77f40408a99549b0
Humberto Rivero <63797901+danieldamian09@users.noreply.github.com>
2021-08-16 11:33:47 -0300

    cambios en logo, footer y mostrar el pais seleccionado de forma dinamica

---
+++ main.js
@@ -0,0 +1,47 @@
+const arg = document.getElementById('argentina');
+const aus = document.getElementById('australia');
+const can = document.getElementById('canada');
+const col = document.getElementById('colombia');
+const chl = document.getElementById('chile');
+const mex = document.getElementById('mexico');
+const eeuu = document.getElementById('unitedStates');
+const world = document.getElementById('world');
+const msg = document.getElementById('elegido');
+console.log(msg)
+
+arg.addEventListener("mouseover", (e) =>{
+    msg.innerHTML = `Argentina`;
+})
+
+aus.addEventListener("mouseover", (e) => {
+    msg.innerHTML = `Australia`;
+})
+
+can.addEventListener("mouseover", (e) => {
+    msg.innerHTML = `Canada`;
+})
+
+col.addEventListener("mouseover", (e) => {
+    msg.innerHTML = `Colombia`;
+})
+
+chl.addEventListener("mouseover", (e) => {
+    msg.innerHTML = `Chile`;
+})
+
+mex.addEventListener("mouseover", (e) => {
+    msg.innerHTML = `Mexico`;
+})
+
+eeuu.addEventListener("mouseover", (e) => {
+    msg.innerHTML = `United States`;
+})
+
+world.addEventListener("mouseover", (e) => {
+    msg.innerHTML = `Rest of the world`;
+})
+
+
+
+
+
